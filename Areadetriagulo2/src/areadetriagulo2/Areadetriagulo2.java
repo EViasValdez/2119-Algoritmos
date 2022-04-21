828fc3d60a35c7bfd390f8316d75d703d1b32103
Eduardo <edyvival@hotmail.com>
2022-04-20 21:49:20 -0500

    Add files via upload

--- Areadetriagulo2/src/areadetriagulo2/Areadetriagulo2.java
+++ Areadetriagulo2/src/areadetriagulo2/Areadetriagulo2.java
@@ -26,8 +26,8 @@ public class Areadetriagulo2 {
         
         System.out.println("Ingresar altura");
         int n2 = leer.nextInt();
-        
         int r;
+        
         r = (n1 * n2) / 2;
         System.out.println("El resultado del area es: " + r);
         {
