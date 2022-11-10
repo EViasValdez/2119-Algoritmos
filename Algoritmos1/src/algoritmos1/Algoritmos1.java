297b5b7a3faec7ce6a4ccf1e97581f360d045ea5
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2022-11-10 10:33:27 -0600

    Add files via upload

---
+++ Algoritmos1/src/algoritmos1/Algoritmos1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmos1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author EViasValdez
+ */
+public class Algoritmos1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer = new Scanner (System.in);
+
+        System.out.println("Numero");
+        int n1 = leer.nextInt();
+
+        if (n1 < 1 && n1 > 3) {
+            System.out.println("Dato incorrecto");
+            }else{
+            System.out.println("Dato correcto");
+        }
+    }
+}
