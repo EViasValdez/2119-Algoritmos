22f0b1d2a975f0b13f4052f7587b5ced698c422c
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-03-27 21:57:47 -0600

    Add files via upload

---
+++ AsesoriasArreglos/src/asesoriasarreglos/AsesoriasArreglos.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package asesoriasarreglos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Eduardo
+ */
+
+public class AsesoriasArreglos {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner Leer = new Scanner (System.in);
+
+        String Nombres [] = new String [7];
+        String Apellidos [] = new String [5];
+
+        for (int i = 1; i <= 7; i++) {
+            System.out.println("Escribe el nombre");
+            Nombres [i] = Leer.next();
+        }
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Escribe el apellido");
+            Apellidos [i] = Leer.next();
+        }
+
+        String Nombre = "Alicia";
+        String Apellido = "Flores";
+
+        for (int i = 1; i < 7; i++) {
+            if (Nombre.equals(Nombres [i])) {
+                System.out.println("Si esta");
+            } else {
+                System.out.println("No esta");
+            }
+        }
+        for (int i = 1; i < 5; i++) {
+            if (Apellido.equals(Apellidos [i])) {
+                System.out.println("Si existe");
+            } else {
+                System.out.println("No existe");
+            }
+        }
+    }
+}
