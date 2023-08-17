e4ddd75047d205d25be9a80b8e6dc951fc1a3c31
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-08-16 20:54:42 -0600

    Add files via upload

--- ZinjaI/Calificaciones.cpp
+++ ZinjaI/Calificaciones.cpp
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
 			{
 				cout << "Calificacion no valida";
 			}
-			else{
+			else {
 			if (Calificacion <= 0 || Calificacion > 10)
 			{
 				cout << "Calificacion no valida";
